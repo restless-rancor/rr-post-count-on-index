11e6448949326c432dc59b30fc216c832e277abd
restless-rancor <44141026+restless-rancor@users.noreply.github.com>
2018-10-20 14:36:08 +0100

    0.0.4-dev
    Added language files

--- event/main_listener.php
+++ event/main_listener.php
@@ -19,30 +19,35 @@ class main_listener implements EventSubscriberInterface
     protected $user;
     protected $template;
 
-    /**
-    * Constructor
-    *
-    * @param \phpbb\user                       $user
-    * @param \phpbb\template\twig\twig $template
-    */
     public function __construct(user $user, twig $template)
     {
         $this->user = $user;
         $this->template = $template;
     }
-	
+
     public static function getSubscribedEvents()
     {
         return array(
+			'core.user_setup'			=> 'load_language_on_setup',
             'core.page_header_after'	=> 'pcoi_header',
         );
     }
-		
+
+	public function load_language_on_setup($event)
+	{
+		$lang_set_ext = $event['lang_set_ext'];
+		$lang_set_ext[] = array(
+			'ext_name' => 'restlessrancor/postcountonindex',
+			'lang_set' => 'postcountonindex',
+		);
+		$event['lang_set_ext'] = $lang_set_ext;
+	}
+
 	public function pcoi_header()
 	{				
 		$this->template->assign_vars([
 			'S_USER_ID'		=> $this->user->data['user_id'],
-			'USER_POST_COUNT'	=> $this->user->data['user_posts'],
+			'USER_POST_COUNT' => $this->user->data['user_posts'],
 		]);
 	}
 }
