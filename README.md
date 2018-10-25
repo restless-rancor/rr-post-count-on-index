840986afbdad29411af3b79c621ef3fbf131a8d0
restless-rancor <44141026+restless-rancor@users.noreply.github.com>
2018-10-25 16:32:02 +0100

    Removed duplicate language call

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-# rr-post-count-on-index
+# Post Count on Index
+
+Displays a logged in member their post count on the index page.
+Clicking the text will search their posts, matching the behavior of memberlist_body.html, memberlist_view.html, viewtopic_body.html etc.
 
 ## Installation
 
 Copy the extension to phpBB/ext/restlessrancor/postcountonindex
 
 Go to "ACP" > "Customise" > "Extensions" and enable the "Post Count on Index" extension.
+
+## phpbb.com development topic
+
+https://www.phpbb.com/community/viewtopic.php?f=456&t=2489951&p=15115641#p15115641
 
 ## License
 
--- event/main_listener.php
+++ event/main_listener.php
@@ -31,23 +31,13 @@ class main_listener implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-			'core.user_setup'			=> 'load_language_on_setup',
             'core.page_header_after'	=> 'pcoi_header',
         );
     }
 
-	public function load_language_on_setup($event)
-	{
-		$lang_set_ext = $event['lang_set_ext'];
-		$lang_set_ext[] = array(
-			'ext_name' => 'restlessrancor/postcountonindex',
-			'lang_set' => 'common',
-		);
-		$event['lang_set_ext'] = $lang_set_ext;
-	}
-
 	public function pcoi_header()
 	{
+		$this->language->add_lang('common', 'restlessrancor/postcountonindex'); 
 		$posts = (int) $this->user->data['user_posts'];		
 		$this->template->assign_vars([
 			'S_USER_ID'		=> $this->user->data['user_id'],
