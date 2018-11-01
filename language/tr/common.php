6a8faeac1f2495b9dde287ded3a4f82c5e391e9a
restless-rancor <44141026+restless-rancor@users.noreply.github.com>
2018-11-01 16:47:42 +0000

    (tr) translation. Push to 1.0.0-RC1 for validation

---
+++ language/tr/common.php
@@ -0,0 +1,25 @@
+<?php
+/**
+*
+* Post Count on Index. An extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018, Restless Rancor, https://www.restlessrancor.com
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+ 
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'USER_POSTS'	=> array(
+		0	=> 'Hiç gönderiniz yok.',
+		1	=> '%d gönderiniz var.',
+		2	=> '%d gönderiniz var!',
+	),
+));
---
+++ language/tr/info_acp_pcoi.php
@@ -0,0 +1,30 @@
+<?php
+/**
+*
+* Post Count on Index. An extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018, Restless Rancor, https://www.restlessrancor.com
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+$lang = array_merge($lang, array(
+	'PCOI_TITLE'			=> 'İndekste Gönderi Sayısı',
+	'PCOI_SETTINGS'			=> 'Ayarlar',
+	'PCOI_ENABLE_FAIL'		=> 'Eklentiyi aktifleştirme başarısız!',
+	'PCOI_ENABLE_SUCCESS'	=> 'Başarıyla aktifleştirildi!',
+	'PCOI_SAVED'			=> 'Ayarlar başarıyla kaydedildi!.',
+	'PCOI_UPDATED'			=> 'İndekste Gönderi Sayısı yapılandırması güncellendi.',
+	
+	'PCOI_ENABLE_EXPLAIN'	=> 'Eğer kapatırsanız gönderi sayıları aşağıda seçtiğiniz konumda <strong>gösterilmeyecek</strong>.',
+	'PCOI_NAVBAR'			=> '<em>navbar</em>da göster?',
+	'PCOI_FORUMLIST'		=> '<em>Forum listesi</em>nin üzerinde göster?',
+	'PCOI_STATISTICS'		=> '<em>Site istatistiklerinde</em>lerinde göster?',
+));
